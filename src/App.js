f1ff19079aa48771428480996cb3f177a3ad6f66
Alan Soto <alansfj348@gmail.com>
2021-08-25 14:31:51 -0500

    Particles component and package added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import ImageLinkForm from "./components/ImageLinkForm";
 import Logo from "./components/Logo";
 import Navigation from "./components/Navigation";
 import Rank from "./components/Rank";
+import Particles from "react-tsparticles";
+import particlesOptions from "./assets/particles.json";
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" options={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
