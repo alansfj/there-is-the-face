9ee2ba5064ec49d157376830af749ee8cf81efe2
Alan Soto <alansfj348@gmail.com>
2021-08-25 12:54:21 -0500

    Logo component added

---
+++ src/components/Logo.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import "../css/Logo.css";
+import face from "../assets/face-recognition.png";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2 br4"
+        options={{ max: 50 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner">
+          <img src={face} alt="face" />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
